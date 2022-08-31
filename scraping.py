00ebec8510e956f1b7e1ede5d6903919506daa15
lell0 <kingolello@gmail.com>
2022-08-31 09:03:10 +0300

    VERSION 6
    - The program prints the scraped link as a one-liner before terminating the program.
    
    - when a requested social media link is not found the value "N/A" will be assigned to the key in the JSON file.

--- scraping.py
+++ scraping.py
@@ -24,36 +24,20 @@ def writeToLogs(logText):
         f.close()
 
 
-def getContentSelenium(url):
+def displayResultFromJson(fileName):
     try:
-        from bs4 import BeautifulSoup
-        from selenium import webdriver
+        f = open(fileName+".json")
+        print(f.read())
     except:
-        writeToLogs("selenium is not installed do 'pip install selenium'")
-        return 0
+        pass
+
+
+def saveFile(dictionary, fileName):
     try:
-        driver = webdriver.Chrome(executable_path='./chromedriver')
-        driver.get(url)
+        with open(fileName+".json", "w") as outfile:
+            json.dump(dictionary, outfile)
     except:
-        writeToLogs(
-            "'chromedriver' executable needs to be in PATH. Please see https://chromedriver.chromium.org/home")
-        return 0
-
-    soup = BeautifulSoup(driver.page_source, features='html.parser')
-    driver.quit()
-
-    return soup
-
-
-def saveFile(dict_, filename):
-
-    json_object = json.dumps(dict_, indent=4)
-
-    with open(filename, "w") as outfile:
-        outfile.write(json_object)
-
-    writeToLogs("Retrieved data saved at: " +
-                str(os.getcwd()) + "/" + filename)
+        pass
 
 
 def xmlToList(fileName):
@@ -62,11 +46,13 @@ def xmlToList(fileName):
         xmlroot = xmlfile.getroot()
         aList = list()
         for child in xmlroot.iter():
-            aList.append(child.text)
+            if child.text.strip():
+                aList.append(child.text)
         return aList
     except:
         writeToLogs("! "+fileName +
                     " was not found, create a file and try again.")
+
 
 def removeDups(dict_):
     try:
@@ -77,7 +63,7 @@ def removeDups(dict_):
                 temp.append(val)
                 result[key] = val
     except:
-        writeToLogs("Something went wrong when removing duplicates")   
+        writeToLogs("Something went wrong when removing duplicates")
     return result
 
 
@@ -91,9 +77,9 @@ def cleanDict(socials):
         for key in whiteList:
 
             if key in social:
+
                 while key in newDict.keys():
                     key += "_"
-
                 try:
                     if socials[social].split("/")[3] in notAProfile:
                         pass
@@ -103,6 +89,10 @@ def cleanDict(socials):
                 except:
                     pass
 
+    newDictKeys = list(newDict.keys())
+    for key in whiteList:
+        if not key in newDictKeys:
+            newDict[key] = "N/A"
     return newDict
 
 
@@ -112,14 +102,15 @@ def getContentSelenium(url):
     try:
         from bs4 import BeautifulSoup
         from selenium import webdriver
-        from selenium.webdriver.chrome.options import Options
+        from selenium.webdriver.firefox.options import Options as FirefoxOptions
     except:
         writeToLogs("selenium is not installed do 'pip install selenium'")
         return 0
     try:
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        driver = webdriver.Chrome(executable_path='./chromedriver',options=chrome_options)
+        options = FirefoxOptions()
+        options.add_argument("--headless")
+        driver = webdriver.Firefox(
+            executable_path='./geckodriver', options=options)
         driver.get(url)
     except:
         writeToLogs(
@@ -166,28 +157,33 @@ def getcontent(url):
 
 
 def scrape(soup):
+    try:
+        tags = soup("a")
+        socials = dict()
+    except:
+        writeToLogs("ERROR while getting tags from soup")
 
-    tags = soup("a")
-    socials = dict()
+    try:
+        for tag in tags:
+            tag = tag.get("href", None)
+            try:
+                key = tag.split("/")[2]
+            except:
+                continue
 
-    for tag in tags:
-        tag = tag.get("href", None)
-        try:
-            key = tag.split("/")[2]
-        except:
-            continue
+            while key in socials.keys():
+                key += "_"
 
-        while key in socials.keys():
-            key += "_"
-
-        socials[key.lower()] = tag.lower()
+            socials[key.lower()] = tag.lower()
+    except:
+        writeToLogs("error splitting tags")
 
     if not socials:
         writeToLogs("! No links were Found")
         return 0
 
-    saveFile(socials, "AllFoundUrls.json")
-    socialsNoDups=removeDups(socials)
+    saveFile(socials, "AllFoundUrls")
+    socialsNoDups = removeDups(socials)
     cleanedDict = cleanDict(socialsNoDups)
 
     if not cleanedDict:
@@ -198,7 +194,16 @@ def scrape(soup):
         )
         exit()
 
-    saveFile(cleanedDict, "CleanOutput.json")
+    emptyDict = True
+    for elm in cleanedDict:
+        if cleanedDict[elm] != "N/A":
+            emptyDict = False
+
+    if emptyDict:
+        saveFile(cleanedDict, "CleanOutput")
+        return 0
+    else:
+        saveFile(cleanedDict, "CleanOutput")
 
 
 def check(url):
@@ -231,34 +236,41 @@ def check(url):
 
 
 def cleanurl(url):
-    if "http://www." in url:
-        url = url.replace("http://www.", "")
-    elif "https://www." in url:
-        url = url.replace("https://www.", "")
-    elif "www." in url:
-        url = url.replace("www.", "")
-    elif "http://" in url:
-        url = url.replace("http://", "")
-    elif "https://" in url:
-        url = url.replace("https://", "")
+    try:
+        if "http://www." in url:
+            url = url.replace("http://www.", "")
+        elif "https://www." in url:
+            url = url.replace("https://www.", "")
+        elif "www." in url:
+            url = url.replace("www.", "")
+        elif "http://" in url:
+            url = url.replace("http://", "")
+        elif "https://" in url:
+            url = url.replace("https://", "")
 
-    writeToLogs("#" * 100)
-    writeToLogs("Target to scrape: ")
-    writeToLogs(url)
-    writeToLogs("#" * 100 + "\n")
+        writeToLogs("#" * 100)
+        writeToLogs("Target to scrape: ")
+        writeToLogs(url)
+        writeToLogs("#" * 100 + "\n")
+    except:
+        writeToLogs("ERROR cleaning the URL")
     return url
 
 
 def checkIfIp(url):
-    result = True
-    match_obj = re.search(r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", url)
-    if match_obj is None:
-        result = False
-    else:
-        for value in match_obj.groups():
-            if int(value) > 255:
-                result = False
-                break
+    try:
+        result = True
+        match_obj = re.search(
+            r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", url)
+        if match_obj is None:
+            result = False
+        else:
+            for value in match_obj.groups():
+                if int(value) > 255:
+                    result = False
+                    break
+    except:
+        writeToLogs("ERROR checking if its an IP")
     return result
 
 
@@ -320,7 +332,7 @@ elif inputType == "domain":
     elif check("http://www." + new_url) < 400:
         if scrape(getcontent("http://www." + new_url)) == 0:
             scrape(getContentSelenium("http://www." + new_url))
-            
+
     elif check("https://www." + new_url) < 400:
         if scrape(getcontent("https://www." + new_url)) == 0:
             scrape(getContentSelenium("https://www." + new_url))
@@ -337,5 +349,7 @@ elif inputType == "domain":
 
 else:
     writeToLogs("\n! WEBSITE NOT ACCESSIBLE, COULDN'T BE SCRAPED\n")
-    
+
+displayResultFromJson("CleanOutput")
+
 writeToLogs("DONE")
